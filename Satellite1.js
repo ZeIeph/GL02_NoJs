5eabc62f65961f409bde5a58fc20c8e5f33939fc
Nathan <nathan.boutevillain@utt.fr>
2023-11-29 10:41:18 +0100

    Retour à l'état initial

--- Satellite1.js
+++
@@ -1,9 +0,0 @@
-var Satellite1 = {
-    check: function(){
-        console.log("Gray is okay".gray)
-        console.log("Blue level checked".blue)
-        console.log("Satellite1 Ready !")
-        return true
-    }
-}
-module.exports = Satellite1
